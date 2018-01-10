0ca87333ba2e864c101ae446afa798ae2b6a4ec2
benqzq <31392185+benqzq@users.noreply.github.com>
2018-01-10 06:03:27 +0200

    Add files via upload

--- man.sh
+++ man.sh
@@ -1,40 +1,28 @@
 # WEBSERVER DEBUGGING:
 
-alias brc="nano /etc/bash.bashrc"
-alias wsm="bash /etc/nginx/sites-available/wsm.sh"
-alias rss="systemctl restart nginx.service"
-alias www="cd /var/www/html"
-alias imb="bash /opt/imb.sh"
-alias dwa="bash /opt/dwa.sh"
+    maldet -u && maldet -d && maldet -a / # u == update. d == details. a == all.
+    cat > /var/www/html/info.php <<< "<?php phpinfo();"
+    netstat -tulpn # List who listens to what on Ubuntu + logins FROM current machine. Grep a port if needed.
+    lsof -i -n -P | grep ssh # List logins TO current machine.
+    ssh USER@IP -vvv -L 22:localhost:22 -L 2222:localhost:8080
 
-maldet -d && maldet -u && maldet -a / # d == details. u == update. a == all.
-cat > /var/www/html/info.php <<< '<?php phpinfo();'
-ssh USER@IP -vvv -L 22:localhost:22 -L 2222:localhost:8080
-# ifconfig || ping || nmap.
-netstat -tulpn # List who listens to what on Ubuntu + logins FROM current machine. Grep a port if needed.
-lsof -i -n -P | grep ssh # List logins TO current machine.
-nginx -t
-tail /var/log/nginx/error.log
-tail /var/log/nginx/access.log
-/etc/init.d/php7.0-fpm restart
-systemctl restart nginx.service
+    printf '\n\n General: \n\n'; nginx -t; printf '\n\n Access: \n\n'; tail /var/log/nginx/access.log; printf '\n\n Errors: \n\n'; tail /var/log/nginx/error.log
+    tail /var/log/dpkg.log file # See what got updated.
+    systemctl restart nginx.service && /etc/init.d/php7.0-fpm restart
+    # sed -i == sed inverbose.
 
 # WEBAPP DEBUGGING:
 
-# DR, DBUN, DBIN, CONFF.
-wp search-replace 'http://' 'https://' --all-tables --allow-root
-wp plugin deactivate --all --allow-root
-wp plugin uninstall xyz --allow-root
-# Power cycle (refreshes iptables).
-# Plugin options (if CSS unloads or if browser gives SSL related warnings).
-
-chown www-data:www-data /var/www/html/* -R &&
-find /var/www/html/* -type d -exec chmod 755 {} \; &&
-find /var/www/html/* -type f -exec chmod 644 {} \;
+    # Check WSR, DIR, DBU, DBI, CONF.
+    wp search-replace "http://" "https://" --all-tables --allow-root
+    wp plugin deactivate --all --allow-root
+    wp plugin uninstall xyz --allow-root
+    # Power cycle (refreshes iptables).
+    # Plugin options (if CSS unloads or if browser gives SSL related warnings).
+    chown www-data:www-data /var/www/html/* -R && find /var/www/html/* -type d -exec chmod 755 {} \; && find /var/www/html/* -type f -exec chmod 644 {} \;
 
 # Wordpress for webapp substate:
 
-# 1. Edit webapp CONFF.
-# 2. Point domain users to hoster DNSs via domain register.
-# 3. Declare domain at hoster.
-# 4. Install Wordpress online.
+    # 2. Point domain users to hoster DNSs via domain register.
+    # 3. Declare domain at hoster.
+    # 4. Install Wordpress online.
